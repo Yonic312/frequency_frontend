c7022abc2246cf60d38b95b5f91734cf396251c6
Yonic312 <ad3121@naver.com>
2025-02-14 01:15:09 +0900

    Add serve package

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,6 @@
-module.exports = {
+export default {
     presets: [
-      "react-app" // React 프로젝트에 맞는 Babel preset
+      'react-app',
     ],
-    plugins: [
-      "@babel/plugin-proposal-private-methods", // private 메서드를 지원하기 위한 플러그인
-      "@babel/plugin-proposal-private-property-in-object" // private 프로퍼티를 객체에서 사용할 수 있도록 해주는 플러그인
-    ]
   };
   
