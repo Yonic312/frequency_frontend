2d2e3d8f2d56ed0ecf7c78cfdf077e9ead75e3cb
Yonic312 <ad3121@naver.com>
2025-02-14 00:33:43 +0900

    Add serve package

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    presets: [
+      // 다른 presets (예: 'react-app' 등 필요한 preset을 추가하세요)
+    ],
+    plugins: [
+      "@babel/plugin-proposal-private-property-in-object",
+      // 다른 플러그인들 (필요한 플러그인을 추가하세요)
+    ]
+  };
+  
